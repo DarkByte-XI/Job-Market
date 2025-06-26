77ec993b2721526e406a1c5b90a5c102ee2cdef9
“DarkByte05” <danichmeis@outlook.fr>
2025-06-26 17:20:26 +0200

    refactor: J'ai rétablie l'appel de load_dotenv à sa forme initiale pour gérer les appels directement de .env

--- src/db/config.py
+++ src/db/config.py
@@ -3,6 +3,8 @@ Ce fichier est nécessaire pour établir une connexion avec la base de données.
 
 !!! Une base de données **PostgresSQL** est strictement nécessaire !!!
 La modélisation ainsi que la partie d'ingestion des données sont strictement propre à Postgres.
+Le dictionnaire de configuration est établie grâce à la documentation postgresql avec les mots clés
+reconnus : https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-PARAMKEYWORDS
 
 Les variables d'environnement sont définies dans le fichier .env.
 Adapter les variables d'environnement pour votre environnement.
@@ -12,9 +14,7 @@ Se référer à la documentation pour plus de détails.
 import os
 from dotenv import load_dotenv
 
-DOTENV_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-dotenv_path = os.path.join(DOTENV_BASE_DIR, "../../.env.jobs_db")
-load_dotenv(dotenv_path = dotenv_path)
+load_dotenv()
 
 
 DB_CONFIG = {
