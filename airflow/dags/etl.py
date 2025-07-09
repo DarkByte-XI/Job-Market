2c8995f5615ea30f6231fa5e7d9404e40564974c
“DarkByte05” <danichmeis@outlook.fr>
2025-07-09 23:02:05 +0200

    feat: ajout du DAG etl.py qui permet de gérer la partie pipelines dans airflow

---
+++ airflow/dags/etl.py
@@ -0,0 +1,47 @@
+from airflow.utils.task_group import TaskGroup
+from airflow import DAG
+from airflow.decorators import task
+from pipelines.extract import extract_from_adzuna, extract_from_ft, extract_from_jsearch
+from pipelines.load import load_jobs_to_db
+from pipelines.transform import (
+    transform_jobs)
+
+
+with DAG("job_market_ETL",
+         catchup = False,
+         schedule = None) as etl_dag:
+
+    with TaskGroup("extract") as extract_group:
+
+        @task(task_id = "extract_adzuna")
+        def extract_adzuna():
+            return extract_from_adzuna()
+
+
+        @task(task_id = "extract_ft")
+        def extract_ft():
+            return extract_from_ft()
+
+
+        @task(task_id = "extract_jsearch")
+        def extract_jsearch():
+            return extract_from_jsearch()
+
+        extract_adzuna = extract_adzuna()
+        extract_ft = extract_ft()
+        extract_jsearch = extract_jsearch()
+
+
+    @task(task_id="transform")
+    def transform_raw_jobs():
+        return transform_jobs()
+
+    transform = transform_raw_jobs()
+
+    @task(task_id="load_to_database")
+    def load_jobs_to_database():
+        return load_jobs_to_db()
+
+    load = load_jobs_to_database()
+
+    extract_group >> transform >> load
