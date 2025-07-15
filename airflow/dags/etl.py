13fcfc438262c3e495b878deeb47e82c29f0dacd
“DarkByte05” <danichmeis@outlook.fr>
2025-07-15 18:20:10 +0200

    feat: ajout d'une tâche sur la base de HttpOperator qui permet de recharger automatiquement les données dans l'API via /reload

--- airflow/dags/etl.py
+++ airflow/dags/etl.py
@@ -1,5 +1,7 @@
+from airflow.providers.http.operators.http import HttpOperator
 from airflow.utils.task_group import TaskGroup
 from datetime import timedelta, datetime
+from time import sleep
 from airflow import DAG
 from airflow.decorators import task
 from pipelines.extract import extract_from_adzuna, extract_from_ft, extract_from_jsearch
@@ -41,10 +43,26 @@ with DAG("job_market_ETL",
 
     transform = transform_raw_jobs()
 
+    @task(task_id="wait_for_file")
+    def wait_for_file():
+        print("Waiting for file to be created...")
+        sleep(10)
+
+    wait = wait_for_file()
+
     @task(task_id="load_to_database")
     def load_jobs_to_database():
         return load_jobs_to_db()
 
     load = load_jobs_to_database()
 
-    etl = extract_group >> transform >> load
+    reload_api = HttpOperator(
+        task_id = "reload_api_data",
+        http_conn_id = "jobs_api",  # Doit être défini dans Airflow (Admin > Connections)
+        method = "POST",
+        endpoint = "/reload",
+        data = {},
+        log_response = True,
+    )
+
+    etl = extract_group >> transform >> wait >> [reload_api, load]
