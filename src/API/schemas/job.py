cc1a5c3cd685e1e6f82df90d16c7bd77992e7619
“DarkByte05” <danichmeis@outlook.fr>
2025-07-30 15:21:24 +0200

    refactor(API): rétablissement du schéma initial

--- src/API/schemas/job.py
+++ src/API/schemas/job.py
@@ -21,12 +21,3 @@ class JobOfferResponse(BaseModel):
     salary_max: Optional[float] = None
     created_at: Optional[datetime] = None
     url: str
-
-class JobOfferUpdate(BaseModel):
-    title: Optional[str]
-    description: Optional[str]
-    location: Optional[str]
-    contract_type: Optional[Literal["CDI", "CDD", "Stage", "Freelance"]]
-    salary_min: Optional[int]
-    salary_max: Optional[int]
-    is_sponsored: Optional[bool]
